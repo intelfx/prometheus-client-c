8cf2e598ebf2864d505e5cb46e7fe93eb65a16fa
Ivan Shapovalov <intelfx@intelfx.name>
2024-01-18 00:49:56 +0100

    Cleanup repetitive code in promhttp_handler()
    Further cleanup promhttp_handler() to reduce the number of repeating
    declarations.

--- promhttp/src/promhttp.c
+++ promhttp/src/promhttp.c
@@ -31,30 +31,32 @@ void promhttp_set_active_collector_registry(prom_collector_registry_t *active_re
 
 enum MHD_Result promhttp_handler(void *cls, struct MHD_Connection *connection, const char *url, const char *method,
                      const char *version, const char *upload_data, size_t *upload_data_size, void **con_cls) {
+  enum MHD_Result ret;
+  struct MHD_Response *response;
+  const char *buf;
+  enum MHD_ResponseMemoryMode mode;
+  unsigned int status_code;
+
   if (strcmp(method, "GET") != 0) {
-    char *buf = "Invalid HTTP Method\n";
-    struct MHD_Response *response = MHD_create_response_from_buffer(strlen(buf), (void *)buf, MHD_RESPMEM_PERSISTENT);
-    enum MHD_Result ret = MHD_queue_response(connection, MHD_HTTP_BAD_REQUEST, response);
-    MHD_destroy_response(response);
-    return ret;
+    buf = "Invalid HTTP Method\n";
+    mode = MHD_RESPMEM_PERSISTENT;
+    status_code = MHD_HTTP_BAD_REQUEST;
+  } else if (strcmp(url, "/") == 0) {
+    buf = "OK\n";
+    mode = MHD_RESPMEM_PERSISTENT;
+    status_code = MHD_HTTP_OK;
+  } else if (strcmp(url, "/metrics") == 0) {
+    buf = prom_collector_registry_bridge(PROM_ACTIVE_REGISTRY);
+    mode = MHD_RESPMEM_MUST_FREE;
+    status_code = MHD_HTTP_OK;
+  } else {
+    buf = "Bad Request\n";
+    mode = MHD_RESPMEM_PERSISTENT;
+    status_code = MHD_HTTP_BAD_REQUEST;
   }
-  if (strcmp(url, "/") == 0) {
-    char *buf = "OK\n";
-    struct MHD_Response *response = MHD_create_response_from_buffer(strlen(buf), (void *)buf, MHD_RESPMEM_PERSISTENT);
-    enum MHD_Result ret = MHD_queue_response(connection, MHD_HTTP_OK, response);
-    MHD_destroy_response(response);
-    return ret;
-  }
-  if (strcmp(url, "/metrics") == 0) {
-    const char *buf = prom_collector_registry_bridge(PROM_ACTIVE_REGISTRY);
-    struct MHD_Response *response = MHD_create_response_from_buffer(strlen(buf), (void *)buf, MHD_RESPMEM_MUST_FREE);
-    enum MHD_Result ret = MHD_queue_response(connection, MHD_HTTP_OK, response);
-    MHD_destroy_response(response);
-    return ret;
-  }
-  char *buf = "Bad Request\n";
-  struct MHD_Response *response = MHD_create_response_from_buffer(strlen(buf), (void *)buf, MHD_RESPMEM_PERSISTENT);
-  enum MHD_Result ret = MHD_queue_response(connection, MHD_HTTP_BAD_REQUEST, response);
+
+  response = MHD_create_response_from_buffer(strlen(buf), (void *)buf, mode);
+  ret = MHD_queue_response(connection, status_code, response);
   MHD_destroy_response(response);
   return ret;
 }
